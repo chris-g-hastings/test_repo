78f8d1d60a02d88f00adec9345db3b9d58ad92b9
Chris Gudde <chrisgudde@hastingsdirect.com>
2020-09-30 15:15:45 +0100

    Added else statment

--- test_python.py
+++ test_python.py
@@ -3,3 +3,5 @@ def print_joke(funny=True):
     print('Why did the chicken cross the road?')
   elif funny=False:
     print('Why do we even ask about chickens?')
+  else:
+    print('Do you even want a joke at all?')
